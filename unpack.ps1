9c6cab3910686de8277371781d650b97742488e2
foobiane <dohertyiane@outlook.com>
2025-02-11 11:22:04 -0600

    Initial commit

---
+++ unpack.ps1
@@ -0,0 +1,8 @@
+﻿param (
+    [string]$path = $(Read-Host "Parent Directory Path")
+)
+
+dir -r $path | % { if (!($_.PsIsContainer)) { $_.FullName } } |
+Foreach-Object {
+    Move-Item -Path $_ -Destination $path
+}
